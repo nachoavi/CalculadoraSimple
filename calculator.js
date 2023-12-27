e1a902a6a10827118a4c44f1ec6460625ce9ca31
nachoavi <luis17.sanmartin@gmail.com>
2023-12-27 20:42:56 -0300

    fixes

--- calculator.js
+++ calculator.js
@@ -18,6 +18,10 @@ function calculate() {
 
 }
 
+function delNumber() {
+    displayValue = displayValue.slice(0,displayValue.length - 1);
+    display.innerText = displayValue
+}
 
 function clearDisplay() {
     displayValue = '';
